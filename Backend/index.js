624335caa23ec6f68ff3bf08933f2271928e805f
Marissa1845 <145675086+Marissa1845@users.noreply.github.com>
2025-01-22 12:19:13 +0530

    First Draft of Listing Functions

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,11 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 
 const uri = "mongodb+srv://<user>:<password>@cluster0.fyvos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
+  autoSelectFamily: false,
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -113,25 +114,143 @@ async function startServer() {
     });
 
     //User Functions
-    app.post('/Register', async (req, res) => {
-       try {
-            await client.db('ReachDB').insertOne(req);
-            res.status(200).send("Registration Successfull!");
-        } catch {
-            console.error('Error registering user: ', error);
-            res.status(500).json({error: 'Failed to register'})
-        }
-    })
-    app.get('/Login', async (req, res) => {
-        try {
-
-        } catch {
-            console.error('Login Failed')
-        }
-    })
+  
     
     //Listing Functions
-    
+    //add a listing    
+    app.post('/newListing', async(req,res)=>{
+      try {
+        const {name,location,type,desc,rating,tags} = req.body;
+        let check = "not found"; //verifyListing(n,loc);
+        if (check==="not found"){
+          await client.db('ReachDB').collection('Listings').insertOne({
+            name: name,
+            location: location,
+            type : type,
+            desc: desc,
+            rating: rating,
+            tags: tags
+          });
+          res.status(200).send('Listing Added!');
+        } else {
+          res.status(400).send('Listing Already Exists!');
+        }
+        
+      } catch (error) {
+        console.error('Invalid Listing: ', error);
+        res.status(500).json({error: 'Something went wrong!'}); //check
+      }
+  })
+
+  //delete a listing - verified
+  app.delete('/removeListing/:id', async(req,res)=>{
+    try{
+      let id = req.params['id']
+      console.log((id))
+      const validID = ObjectId.isValid(id) ? new ObjectId(id):null;
+      if (validID){
+        const result = await client.db('ReachDB').collection('Listings').deleteOne({"_id":validID}); 
+        if (result.deletedCount===1){
+          res.status(200).send("Successfully deleted!");
+        }else{
+          res.status(400).send("Record not Found");
+        }
+        
+      }else{
+        res.status(400).send("Invalid ID");
+      }
+      
+      
+    } catch(err) {
+      console.error('Unable to Delete Listing: ',err);
+      res.status(500).json({error: 'Something went wrong!'})
+    }
+  })
+
+
+  //view all listings of one category - verified
+  app.get('/search/:type',async (req,res)=>{
+    try{
+      const list = await client.db('ReachDB').collection('Listings').find({type:req.params['type']}).toArray();
+      list.length!==0? res.status(200).send(list): res.status(400).send("Not Found");
+    } catch (err){
+      console.log('Failed to retrieve:',err)
+      res.status(500).json({error: 'Something went wrong!'})
+    }
+  })
+
+  //view one listing
+  /*
+  app.get('/search/:id',async(req,res)=>{
+    try{
+      const listing = client.db('ReachDB').collection('Listings').find({"_id":req.params['id']}).toArray();
+      listing.length===1?res.status(200).send(listing): res.status(400).send("Not Found")
+    } catch {
+      console.error('Failed to retrieve: ', error);
+      res.status(500).json({error: 'Something went wrong!'});
+    }
+  })*/
+
+  //search for listing - name, tags, type - 2 mistakes - verified
+  app.get('/search',async (req,res)=>{ 
+    try{
+      let query = req.query.query;
+      console.log(query);
+      const collection = await client.db("ReachDB").collection("Listings");
+
+      const docs = await collection.aggregate([
+       {
+        $search: {
+          "text": {
+          "query": query,
+          "path": ["name","tags","type"],
+          "fuzzy":{
+            "maxEdits":2
+          }
+          }
+        }
+      }
+    ]).toArray();
+    console.log(docs);  
+    docs.length!==0?res.status(200).send(docs):res.status(400).send("Not found")
+          
+   } catch (error) {
+      console.error('Failed to retrieve: ', error);
+      res.status(500).json({error: 'Something went wrong!'})
+    }
+  })
+
+  //update listing - verified
+  app.patch('/updateListing/:id',async (req,res)=>{
+    try{
+      let id = req.params['id']
+      const validID = ObjectId.isValid(id) ? ObjectId.createFromHexString(id):null;
+      console.log(req.body);
+      const {name, type, tags} = req.body;
+      
+
+      if (validID){
+        const result = await client.db('ReachDB').collection('Listings').updateOne(
+          {"_id":validID},
+          {
+            $set: {
+              name: name,
+              type: type,
+              tags: tags
+            }
+          }
+        ); 
+        console.log("Updated");
+        
+      }else{
+        res.status(400).send("Invalid ID");
+      }
+      
+    } catch (err){
+      console.log('Unable to Update:',err)
+      res.status(500).json({error: 'Something went wrong!'})
+    }
+  })
 
     //Review Functions
 

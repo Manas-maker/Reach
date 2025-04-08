de3655caed3d2d030e13bc6532ec3b0a0edb5db8
Manas Pradhan <64654573+Manas-maker@users.noreply.github.com>
2025-04-08 10:51:45 +0530

    setting fixed width to the page

--- Frontend/src/Components/LoadingScreen.jsx
+++ Frontend/src/Components/LoadingScreen.jsx
@@ -1,9 +1,9 @@
 import "./LoadingScreen.css"
 const LoadingScreen=()=>{
-    return(<>
+    return(<div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
         <img src='/bee-cute.gif' style={{marginTop:"50vh", transform:"translateY(-50%)"}}/>
         <h1 id="loadingReaching" style={{textAlign:"center"}}></h1>
-        </>
+        </div>
     )
 }
 
